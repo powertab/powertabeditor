d3a8950d9c1159077ce0e4df4267d6cca2fe8d2d
Pablo Popcorn <datenfunk@t-online.de>
2010-11-22 17:12:12 +0000

    More of the same, working on the Toolbox widget!
    git-svn-id: http://svn.powertabs.org/svn/ptedev/trunk@25 5b1460fe-822e-4aa1-ac1b-8a6412c92c8c

--- source/toolbox.cpp
+++ source/toolbox.cpp
@@ -1,7 +1,6 @@
 #include "toolbox.h"
 
 #include <QHBoxLayout>
-#include <QPushButton>
 #include <QLabel>
 
 Toolbox::Toolbox(QWidget *parent) :
@@ -16,56 +15,88 @@ Toolbox::Toolbox(QWidget *parent) :
 	addTab(new ScoreTab, QIcon(":/icons/toolbox_score.png"),"");
 }
 
+void NoteTab::ResetNoteAndRestButtons()
+{
+	note1_button->setChecked(false);
+	note2_button->setChecked(false);
+	note4_button->setChecked(false);
+	note8_button->setChecked(false);
+	note16_button->setChecked(false);
+	note32_button->setChecked(false);
+
+	rest1_button->setChecked(false);
+	rest2_button->setChecked(false);
+	rest4_button->setChecked(false);
+	rest8_button->setChecked(false);
+	rest16_button->setChecked(false);
+	rest32_button->setChecked(false);
+}
+
 NoteTab::NoteTab(QWidget *parent) : QWidget(parent)
 {
 	QHBoxLayout *noteRow = new QHBoxLayout;
-	QPushButton *button1 = new QPushButton;
-	button1->setText("1");
-	button1->setCheckable(true);
-	button1->setChecked(true);
-	noteRow->addWidget(button1);
-	QPushButton *button2 = new QPushButton;
-	button2->setText("2");
-	button2->setCheckable(true);
-	noteRow->addWidget(button2);
-	QPushButton *button3 = new QPushButton;
-	button3->setText("4");
-	button3->setCheckable(true);
-	noteRow->addWidget(button3);
-	QPushButton *button4 = new QPushButton;
-	button4->setText("8");
-	button4->setCheckable(true);
-	noteRow->addWidget(button4);
-	QPushButton *button5 = new QPushButton;
-	button5->setText("16");
-	button5->setCheckable(true);
-	noteRow->addWidget(button5);
-	QPushButton *button6 = new QPushButton;
-	button6->setText("32");
-	button6->setCheckable(true);
-	noteRow->addWidget(button6);
+	note1_button = new QPushButton(QIcon(":/images/whole_note.gif"),"");
+	note1_button->setCheckable(true);
+	note1_button->setChecked(true);
+	noteRow->addWidget(note1_button);
+	note2_button = new QPushButton(QIcon(":/images/half_note.gif"),"");
+	note2_button->setCheckable(true);
+	noteRow->addWidget(note2_button);
+	note4_button = new QPushButton(QIcon(":/images/quarter_note.gif"),"");
+	note4_button->setCheckable(true);
+	noteRow->addWidget(note4_button);
+	note8_button = new QPushButton(QIcon(":/images/8th_note.gif"),"");
+	note8_button->setCheckable(true);
+	noteRow->addWidget(note8_button);
+	note16_button = new QPushButton(QIcon(":/images/16th_note.gif"),"");
+	note16_button->setCheckable(true);
+	noteRow->addWidget(note16_button);
+	note32_button =new QPushButton(QIcon(":/images/32th_note.gif"),"");
+	note32_button->setCheckable(true);
+	noteRow->addWidget(note32_button);
 
 	QHBoxLayout *restRow = new QHBoxLayout;
-	QPushButton *rbutton1 = new QPushButton;
-	rbutton1->setText("R1");
-	restRow->addWidget(rbutton1);
-	QPushButton *rbutton2 = new QPushButton;
-	rbutton2->setText("R2");
-	restRow->addWidget(rbutton2);
-	QPushButton *rbutton3 = new QPushButton;
-	rbutton3->setText("R4");
-	restRow->addWidget(rbutton3);
-	QPushButton *rbutton4 = new QPushButton;
-	rbutton4->setText("R8");
-	restRow->addWidget(rbutton4);
-	QPushButton *rbutton5 = new QPushButton;
-	rbutton5->setText("R16");
-	restRow->addWidget(rbutton5);
-	QPushButton *rbutton6 = new QPushButton;
-	rbutton6->setText("R32");
-	restRow->addWidget(rbutton6);
+	rest1_button = new QPushButton;
+	rest1_button->setText("1");
+	rest1_button->setCheckable(true);
+	restRow->addWidget(rest1_button);
+	rest2_button = new QPushButton;
+	rest2_button->setText("2");
+	rest2_button->setCheckable(true);
+	restRow->addWidget(rest2_button);
+	rest4_button = new QPushButton;
+	rest4_button->setText("4");
+	rest4_button->setCheckable(true);
+	restRow->addWidget(rest4_button);
+	rest8_button = new QPushButton;
+	rest8_button->setText("8");
+	rest8_button->setCheckable(true);
+	restRow->addWidget(rest8_button);
+	rest16_button = new QPushButton;
+	rest16_button->setText("16");
+	rest16_button->setCheckable(true);
+	restRow->addWidget(rest16_button);
+	rest32_button = new QPushButton;
+	rest32_button->setText("32");
+	rest32_button->setCheckable(true);
+	restRow->addWidget(rest32_button);
+
+	connect(note1_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(note2_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(note4_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(note8_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(note16_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(note32_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+
+	connect(rest1_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(rest2_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(rest4_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(rest8_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(rest16_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
+	connect(rest32_button,SIGNAL(pressed()),this,SLOT(ResetNoteAndRestButtons()));
 
 	QVBoxLayout *mainLayout = new QVBoxLayout;
+	mainLayout->addSpacing(5);
 	mainLayout->addLayout(noteRow);
 	mainLayout->addLayout(restRow);
 	mainLayout->addStretch(1);
--- source/toolbox.h
+++ source/toolbox.h
@@ -3,6 +3,7 @@
 
 #include <QTabWidget>
 #include <QToolBox>
+#include <QPushButton>
 #include <QVBoxLayout>
 
 class NoteTab : public QWidget
@@ -11,6 +12,24 @@ class NoteTab : public QWidget
 
 public:
 	NoteTab(QWidget *parent = 0);
+
+private:
+	QPushButton *note1_button;
+	QPushButton *note2_button;
+	QPushButton *note4_button;
+	QPushButton *note8_button;
+	QPushButton *note16_button;
+	QPushButton *note32_button;
+
+	QPushButton *rest1_button;
+	QPushButton *rest2_button;
+	QPushButton *rest4_button;
+	QPushButton *rest8_button;
+	QPushButton *rest16_button;
+	QPushButton *rest32_button;
+
+private slots:
+	void ResetNoteAndRestButtons();
 };
 
 class OrnamentTab : public QWidget
