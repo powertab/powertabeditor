c25b7bb63f581b032ea6b3e9ccfe0feaa3130f7c
Pablo Popcorn <datenfunk@t-online.de>
2010-11-27 23:52:45 +0000

    Trying out the new and improved build system. :D Just some cosmetic work on the default skin.
    git-svn-id: http://svn.powertabs.org/svn/ptedev/trunk@39 5b1460fe-822e-4aa1-ac1b-8a6412c92c8c

--- source/main.cpp
+++ source/main.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    PowerTabEditor w;
-    w.show();
+	PowerTabEditor w;
+	w.show();
 
-    return a.exec();
+	return a.exec();
 }
