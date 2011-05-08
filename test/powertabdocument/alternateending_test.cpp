a8444f419065ee4128586e9f25f03b477ee9fde0
Cameron White <cameronwhite91@gmail.com>
2011-05-08 17:37:06 +0000

    Added test case for AlternateEnding::GetListOfNumbers
    git-svn-id: http://svn.powertabs.org/svn/ptedev/trunk@407 5b1460fe-822e-4aa1-ac1b-8a6412c92c8c

--- test/powertabdocument/alternateending_test.cpp
+++ test/powertabdocument/alternateending_test.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_DYN_LINK
 
 #include <boost/test/unit_test.hpp>
+#include <boost/foreach.hpp>
 
 #include <powertabdocument/alternateending.h>
 
@@ -24,6 +25,27 @@ BOOST_AUTO_TEST_SUITE(AlternateEndingTest)
 
         ending.SetNumbers(1 | 16 | 32 | 64 | 512);
         BOOST_CHECK_EQUAL(ending.GetText(), "1., 5.-7., D.S.");
+    }
+
+    BOOST_AUTO_TEST_CASE(GetListOfNumbers)
+    {
+        AlternateEnding ending;
+
+        std::vector<uint8_t> originalNumbers;
+        originalNumbers.push_back(1);
+        originalNumbers.push_back(3);
+        originalNumbers.push_back(4);
+        originalNumbers.push_back(7);
+
+        BOOST_FOREACH(uint8_t number, originalNumbers)
+        {
+            ending.SetNumber(number);
+        }
+
+        std::vector<uint8_t> numbers = ending.GetListOfNumbers();
+
+        BOOST_CHECK_EQUAL_COLLECTIONS(numbers.begin(), numbers.end(),
+                                      originalNumbers.begin(), originalNumbers.end());
     }
 
 BOOST_AUTO_TEST_SUITE_END()
