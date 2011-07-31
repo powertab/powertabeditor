7e6e12d81984322eccea4072fb92f4219baaaf3d
Cameron White <cameronwhite91@gmail.com>
2011-07-31 03:13:52 +0000

    Guitar Pro 6 - added support for importing the MasterBar data
    git-svn-id: http://svn.powertabs.org/svn/ptedev/trunk@535 5b1460fe-822e-4aa1-ac1b-8a6412c92c8c

--- source/formats/gpx/documentreader.cpp
+++ source/formats/gpx/documentreader.cpp
@@ -2,14 +2,13 @@
 
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/foreach.hpp>
+#include <boost/spirit/include/qi.hpp>
 
 #include <powertabdocument/powertabdocument.h>
 #include <powertabdocument/score.h>
 #include <powertabdocument/guitar.h>
-
-#include <sstream>
-
-using boost::property_tree::ptree;
+#include <powertabdocument/barline.h>
+#include <powertabdocument/system.h>
 
 Gpx::DocumentReader::DocumentReader(const std::string& xml)
 {
@@ -22,6 +21,7 @@ void Gpx::DocumentReader::readDocument(std::shared_ptr<PowerTabDocument> doc)
 {
     readHeader(doc->GetHeader());
     readTracks(doc->GetGuitarScore());
+    readBars();
 }
 
 /// Loads the header information (song title, artist, etc)
@@ -38,6 +38,24 @@ void Gpx::DocumentReader::readHeader(PowerTabFileHeader& header)
 
     header.SetSongGuitarScoreTranscriber(gpHeader.get<std::string>("Tabber"));
     header.SetSongGuitarScoreNotes(gpHeader.get<std::string>("Instructions"));
+}
+
+namespace
+{
+/// Utility function for converting a string of space-separated values to a vector of that type
+template <typename T>
+void convertStringToList(const std::string& source, std::vector<T>& dest)
+{
+    using namespace boost::spirit::qi;
+
+    bool parsed = parse(source.begin(), source.end(),
+                        auto_ % ' ', dest);
+
+    if (!parsed)
+    {
+        std::cerr << "Parsing of list failed!!" << std::endl;
+    }
+}
 }
 
 /// Imports the tracks and performs a conversion to the PowerTab Guitar class
@@ -60,11 +78,9 @@ void Gpx::DocumentReader::readTracks(Score *score)
         {
             // Read the tuning - need to convert from a string of numbers separated by spaces to
             // a vector of integers
-            std::stringstream tuningString (properties->get<std::string>("Property.Pitches"));
-
+            const std::string tuningString = properties->get<std::string>("Property.Pitches");
             std::vector<uint8_t> tuningNotes;
-            std::copy(std::istream_iterator<int>(tuningString), std::istream_iterator<int>(),
-                      std::back_inserter(tuningNotes));
+            convertStringToList(tuningString, tuningNotes);
 
             guitar->GetTuning().SetTuningNotes(tuningNotes);
 
@@ -73,5 +89,68 @@ void Gpx::DocumentReader::readTracks(Score *score)
         }
 
         score->InsertGuitar(guitar);
+    }
+}
+
+void Gpx::DocumentReader::readBars()
+{
+    BOOST_FOREACH(const ptree::value_type& node, gpFile.get_child("GPIF.MasterBars"))
+    {
+        if (node.first != "MasterBar")
+        {
+            continue;
+        }
+
+        const ptree& masterBar = node.second;
+
+        System::BarlinePtr barline = std::make_shared<Barline>();
+
+        readKeySignature(masterBar, barline->GetKeySignature());
+        readTimeSignature(masterBar, barline->GetTimeSignature());
+
+        std::vector<int> barIds;
+        convertStringToList(masterBar.get<std::string>("Bars"), barIds);
+    }
+}
+
+void Gpx::DocumentReader::readKeySignature(const Gpx::DocumentReader::ptree& masterBar, KeySignature& key)
+{
+    key.SetKeyAccidentals(masterBar.get<uint8_t>("Key.AccidentalCount"));
+
+    const std::string keyType = masterBar.get<std::string>("Key.Mode");
+
+    if (keyType == "Major")
+    {
+        key.SetKeyType(KeySignature::majorKey);
+    }
+    else if (keyType == "Minor")
+    {
+        key.SetKeyType(KeySignature::minorKey);
+    }
+    else
+    {
+        std::cerr << "Unknown key type" << std::endl;
+    }
+}
+
+void Gpx::DocumentReader::readTimeSignature(const Gpx::DocumentReader::ptree& masterBar,
+                                            TimeSignature& timeSignature)
+{
+    const std::string timeString = masterBar.get<std::string>("Time");
+
+    std::vector<int> timeSigValues;
+    using namespace boost::spirit::qi;
+
+    // import time signature (stored in text format - e.g. "4/4")
+    bool parsed = parse(timeString.begin(), timeString.end(),
+                        int_ >> '/' >> int_, timeSigValues);
+
+    if (!parsed)
+    {
+        std::cerr << "Parsing of time signature failed!!" << std::endl;
+    }
+    else
+    {
+        timeSignature.SetMeter(timeSigValues[0], timeSigValues[1]);
     }
 }
--- source/formats/gpx/documentreader.h
+++ source/formats/gpx/documentreader.h
@@ -2,12 +2,13 @@
 #define GPX_DOCUMENTREADER_H
 
 #include <memory>
-#include <iosfwd>
 #include <boost/property_tree/ptree.hpp>
 
 class PowerTabDocument;
 class PowerTabFileHeader;
 class Score;
+class KeySignature;
+class TimeSignature;
 
 namespace Gpx
 {
@@ -20,10 +21,15 @@ public:
     void readDocument(std::shared_ptr<PowerTabDocument> doc);
 
 private:
-    boost::property_tree::ptree gpFile;
+    typedef boost::property_tree::ptree ptree;
+
+    ptree gpFile;
 
     void readHeader(PowerTabFileHeader& header);
     void readTracks(Score* score);
+    void readBars();
+    void readKeySignature(const ptree& masterBar, KeySignature& key);
+    void readTimeSignature(const ptree& masterBar, TimeSignature& timeSignature);
 };
 
 }
