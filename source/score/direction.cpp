00e4affdc87b13e0b7e42003a13e792dad645b70
Cameron White <cameronwhite91@gmail.com>
2013-07-11 03:03:41 +0000

    Add the new Direction class. The interface has a few changes to make it simpler to iterate through the symbols.
    git-svn-id: http://svn.powertabs.org/svn/ptedev/trunk@935 5b1460fe-822e-4aa1-ac1b-8a6412c92c8c

---
+++ source/score/direction.cpp
@@ -0,0 +1,101 @@
+/*
+  * Copyright (C) 2013 Cameron White
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "direction.h"
+
+namespace Score {
+
+Direction::Direction()
+    : myPosition(0)
+{
+}
+
+bool Direction::operator==(const Direction &other) const
+{
+    return myPosition == other.myPosition &&
+           mySymbols == other.mySymbols;
+}
+
+int Direction::getPosition() const
+{
+    return myPosition;
+}
+
+void Direction::setPosition(int position)
+{
+    myPosition = position;
+}
+
+boost::iterator_range<Direction::SymbolIterator> Direction::getSymbols()
+{
+    return boost::make_iterator_range(mySymbols);
+}
+
+boost::iterator_range<Direction::SymbolConstIterator> Direction::getSymbols() const
+{
+    return boost::make_iterator_range(mySymbols);
+}
+
+void Direction::insertSymbol(const DirectionSymbol &symbol)
+{
+    mySymbols.push_back(symbol);
+}
+
+void Direction::removeSymbol(const DirectionSymbol &symbol)
+{
+    mySymbols.erase(std::remove(mySymbols.begin(), mySymbols.end(), symbol),
+                    mySymbols.end());
+}
+
+DirectionSymbol::DirectionSymbol()
+    : mySymbolType(Coda),
+      myActiveSymbolType(ActiveNone),
+      myRepeatNumber(0)
+{
+}
+
+DirectionSymbol::DirectionSymbol(SymbolType type, ActiveSymbolType activeType,
+                                 int repeatNumber)
+    : mySymbolType(type),
+      myActiveSymbolType(activeType),
+      myRepeatNumber(repeatNumber)
+{
+}
+
+bool DirectionSymbol::operator==(const DirectionSymbol &other) const
+{
+    return mySymbolType == other.mySymbolType &&
+           myActiveSymbolType == other.myActiveSymbolType &&
+           myRepeatNumber == other.myRepeatNumber;
+}
+
+DirectionSymbol::SymbolType DirectionSymbol::getSymbolType() const
+{
+    return mySymbolType;
+}
+
+DirectionSymbol::ActiveSymbolType DirectionSymbol::getActiveSymbolType() const
+{
+    return myActiveSymbolType;
+}
+
+int DirectionSymbol::getRepeatNumber() const
+{
+    return myRepeatNumber;
+}
+
+}
---
+++ source/score/direction.h
@@ -0,0 +1,125 @@
+/*
+  * Copyright (C) 2013 Cameron White
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SCORE_DIRECTION_H
+#define SCORE_DIRECTION_H
+
+#include <boost/range/iterator_range_core.hpp>
+#include <boost/serialization/access.hpp>
+#include <vector>
+
+namespace Score {
+
+class DirectionSymbol
+{
+public:
+    enum SymbolType
+    {
+        Coda,
+        DoubleCoda,
+        Segno,
+        SegnoSegno,
+        Fine,
+        DaCapo,
+        DalSegno,
+        DalSegnoSegno,
+        ToCoda,
+        ToDoubleCoda,
+        DaCapoAlCoda,
+        DaCapoAlDoubleCoda,
+        DalSegnoAlCoda,
+        DalSegnoAlDoubleCoda,
+        DalSegnoSegnoAlCoda,
+        DalSegnoSegnoAlDoubleCoda,
+        DaCapoAlFine,
+        DalSegnoAlFine,
+        DalSegnoSegnoAlFine
+    };
+
+    enum ActiveSymbolType
+    {
+        ActiveNone,
+        ActiveDaCapo,
+        ActiveDalSegno,
+        ActiveDalSegnoSegno
+    };
+
+    DirectionSymbol();
+    DirectionSymbol(SymbolType type, ActiveSymbolType activeType = ActiveNone,
+                    int repeatNumber = 0);
+    bool operator==(const DirectionSymbol &other) const;
+
+    /// Return the type of this symbol.
+    SymbolType getSymbolType() const;
+    /// Return the symbol type that this symbol is active for.
+    ActiveSymbolType getActiveSymbolType() const;
+    /// Return the repeat number that this symbol is active on.
+    int getRepeatNumber() const;
+
+private:
+    SymbolType mySymbolType;
+    ActiveSymbolType myActiveSymbolType;
+    int myRepeatNumber;
+
+    friend class boost::serialization::access;
+    template <class Archive>
+    void serialize(Archive &ar, const unsigned int /*version*/)
+    {
+        ar & mySymbolType & myActiveSymbolType & myRepeatNumber;
+    }
+};
+
+class Direction
+{
+public:
+    typedef std::vector<DirectionSymbol>::iterator SymbolIterator;
+    typedef std::vector<DirectionSymbol>::const_iterator SymbolConstIterator;
+
+    Direction();
+
+    bool operator==(const Direction &other) const;
+
+    /// Returns the position within the system where the direction is anchored.
+    int getPosition() const;
+    /// Sets the position within the system where the direction is anchored.
+    void setPosition(int position);
+
+    /// Returns the set of symbols in the direction.
+    boost::iterator_range<SymbolIterator> getSymbols();
+    /// Returns the set of symbols in the direction.
+    boost::iterator_range<SymbolConstIterator> getSymbols() const;
+
+    /// Adds a new symbol to the direction.
+    void insertSymbol(const DirectionSymbol &symbol);
+    /// Removes a symbol from the direction.
+    void removeSymbol(const DirectionSymbol &symbol);
+
+private:
+    int myPosition;
+    std::vector<DirectionSymbol> mySymbols;
+
+    friend class boost::serialization::access;
+    template <class Archive>
+    void serialize(Archive &ar, const unsigned int /*version*/)
+    {
+        ar & myPosition & mySymbols;
+    }
+};
+
+}
+
+#endif
