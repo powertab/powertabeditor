4edb8495be1ac75c4314d623effa98cc3d3d958f
Cameron White <cameronwhite91@gmail.com>
2013-07-17 02:21:56 +0000

    Add missing test from the previous commit.
    git-svn-id: http://svn.powertabs.org/svn/ptedev/trunk@999 5b1460fe-822e-4aa1-ac1b-8a6412c92c8c

---
+++ test/actions/test_addsystem.cpp
@@ -0,0 +1,40 @@
+/*
+  * Copyright (C) 2013 Cameron White
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+  
+#include <catch.hpp>
+
+#include <actions/addsystem.h>
+#include <score/score.h>
+
+TEST_CASE("Actions/AddSystem", "")
+{
+    Score score;
+    System system;
+    score.insertSystem(system);
+    score.insertSystem(system);
+
+    AddSystem action(score, 1);
+
+    action.redo();
+    REQUIRE(score.getSystems().size() == 3);
+    REQUIRE(score.getSystems()[0].getStaves().size() == 0);
+    REQUIRE(score.getSystems()[1].getStaves().size() == 1);
+    REQUIRE(score.getSystems()[2].getStaves().size() == 0);
+
+    action.undo();
+    REQUIRE(score.getSystems().size() == 2);
+}
