powertab/powertabeditor

commit 8cd52ef5da0aec725d9d1f51bba95eabe44c4c9e

- Fix a bug that caused score merging to fail on measures that were empty apart from e.g. a tempo marker.
- Update the v1.7 tests for score merging.

git-svn-id: http://svn.powertabs.org/svn/ptedev/trunk@1318 5b1460fe-822e-4aa1-ac1b-8a6412c92c8c

source/score/utils/scoremerger.cpp
@@ -549,9 +549,17 @@ ScoreMerger::State::State(Score &score, bool isBass)
     bool empty = true;
     for (const Staff &staff : loc.getSystem().getStaves())
     {
+        empty &= staff.getDynamics().empty();
+
         for (const Voice &voice : staff.getVoices())
             empty &= voice.getPositions().empty();
     }
+
+    empty &= loc.getSystem().getTempoMarkers().empty();
+    empty &= loc.getSystem().getAlternateEndings().empty();
+    empty &= loc.getSystem().getChords().empty();
+    empty &= loc.getSystem().getDirections().empty();
+    empty &= loc.getSystem().getPlayerChanges().empty();
 
     // If it looks like the score is unused, don't do anything.
     if (empty)

test/formats/powertab_old/test_powertabold.cpp
@@ -50,14 +50,17 @@ TEST_CASE("Formats/PowerTabOldImport/Guitars", "")
     PowerTabOldImporter importer;
     importer.load("data/guitars.ptb", score);
 
-    REQUIRE(score.getPlayers().size() == 2);
-    REQUIRE(score.getInstruments().size() == 2);
+    // Should contain two players from the guitar score and one from the bass score.
+    REQUIRE(score.getPlayers().size() == 3);
+    REQUIRE(score.getInstruments().size() == 3);
 
     const Player &player1 = score.getPlayers()[0];
     const Player &player2 = score.getPlayers()[1];
+    const Player &player3 = score.getPlayers()[2];
     REQUIRE(player1.getDescription() == "First Player");
     REQUIRE(player2.getDescription() == "Second Player");
     REQUIRE(player2.getTuning().getStringCount() == 7);
+    REQUIRE(player3.getTuning().getStringCount() == 4);
 
     REQUIRE(score.getInstruments()[0].getDescription() == "Electric Guitar (clean)");
 }
@@ -176,7 +179,7 @@ TEST_CASE("Formats/PowerTabOldImport/Staves", "")
 
     const System &system = score.getSystems()[0];
 
-    REQUIRE(system.getStaves().size() == 2);
+    REQUIRE(system.getStaves().size() == 3);
     const Staff &staff1 = system.getStaves()[0];
     const Staff &staff2 = system.getStaves()[1];
 

commit e44c0ba6931945b0e2e141dc5ac3ba5cc8043031

Fix some issues introduced by r1318 - we should really only check that there are notes somewhere in the score. Previously, the default empty bass score from v1.7 (which only contained a Guitar In) was being treated as non-empty.
git-svn-id: http://svn.powertabs.org/svn/ptedev/trunk@1321 5b1460fe-822e-4aa1-ac1b-8a6412c92c8c

source/score/utils/scoremerger.cpp
@@ -547,19 +547,17 @@ ScoreMerger::State::State(Score &score, bool isBass)
       finishing(false)
 {
     bool empty = true;
-    for (const Staff &staff : loc.getSystem().getStaves())
+    for (const System &system : score.getSystems())
     {
-        empty &= staff.getDynamics().empty();
+        for (const Staff &staff : system.getStaves())
+        {
+            for (const Voice &voice : staff.getVoices())
+                empty &= voice.getPositions().empty();
+        }
 
-        for (const Voice &voice : staff.getVoices())
-            empty &= voice.getPositions().empty();
+        if (!empty)
+            break;
     }
-
-    empty &= loc.getSystem().getTempoMarkers().empty();
-    empty &= loc.getSystem().getAlternateEndings().empty();
-    empty &= loc.getSystem().getChords().empty();
-    empty &= loc.getSystem().getDirections().empty();
-    empty &= loc.getSystem().getPlayerChanges().empty();
 
     // If it looks like the score is unused, don't do anything.
     if (empty)