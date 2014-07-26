91c37ca1a749dc6631533922f8f4dc5e90761a89
Cameron White <cameronwhite91@gmail.com>
2014-07-26 16:56:12 -0400

    Removed some very outdated build scripts and reorganized files a bit.

--- build/windows_installer/build_installer.bat
+++
@@ -1,7 +0,0 @@
-rem Replace with the path to your Wix installation directory.
-set WIX_BIN="C:\Program Files (x86)\Wix Toolset v3.7\bin"
-
-%WIX_BIN%"\candle.exe" pte_windows_build.wxs
-%WIX_BIN%"\light.exe" -out powertabeditor.msi pte_windows_build.wixobj
-
-pause
