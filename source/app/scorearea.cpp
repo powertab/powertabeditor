powertab/powertabeditor

commit d904aaf12b6036a1ee52579719c11882b36c0131

Initial work on multithreaded rendering.
This is currently disabled for now until more performance testing is
done.

source/app/scorearea.cpp
@@ -20,13 +20,13 @@
 #include <app/documentmanager.h>
 #include <app/pubsub/scorelocationpubsub.h>
 #include <app/pubsub/staffpubsub.h>
-#include <boost/timer.hpp>
+#include <chrono>
 #include <painters/caretpainter.h>
 #include <painters/systemrenderer.h>
 #include <QDebug>
 #include <QGraphicsItem>
-#include <QProgressDialog>
 #include <score/score.h>
+#include <thread>
 
 static const double SYSTEM_SPACING = 50;
 
@@ -51,42 +51,65 @@ void ScoreArea::renderDocument(const Document &document, Staff::ViewType view)
 
     const Score &score = document.getScore();
 
-    boost::timer timer;
-    QProgressDialog progressDialog(tr("Rendering ..."), "", 0,
-                                   score.getSystems().size());
-    progressDialog.setCancelButton(nullptr);
-    progressDialog.setWindowModality(Qt::WindowModal);
-    progressDialog.show();
+    auto start = std::chrono::high_resolution_clock::now();
 
     myCaretPainter = new CaretPainter(document.getCaret());
-
     myCaretPainter->subscribeToMovement([=]() {
         adjustScroll();
     });
 
-    // Render each system.
+    myRenderedSystems.reserve(score.getSystems().size());
+    for (const System &system : score.getSystems())
+        myRenderedSystems.append(nullptr);
+
+#if 0
+    const int numThreads = std::thread::hardware_concurrency();
+#else
+    const int numThreads = 1;
+#endif
+
+    std::vector<std::thread> workers;
+    const int workSize = myRenderedSystems.size() / numThreads;
+    qDebug() << "Using " << numThreads << " worker thread(s)";
+
+    for (int i = 0; i < numThreads; ++i)
+    {
+        const int left = i * workSize;
+        const int right = (i == numThreads - 1) ? myRenderedSystems.size()
+                                                : (i + 1) * workSize;
+
+        workers.emplace_back([&](int left, int right)
+        {
+            for (int i = left; i < right; ++i)
+            {
+                SystemRenderer render(this, score);
+                myRenderedSystems[i] = render(score.getSystems()[i], i, myViewType);
+            }
+        }, left, right);
+    }
+
+    for (std::thread &worker : workers)
+        worker.join();
+
     int i = 0;
     double height = 0;
-    for (const System &system : score.getSystems())
+    // Layout the systems.
+    for (QGraphicsItem *system : myRenderedSystems)
     {
-        progressDialog.setValue(i);
-        SystemRenderer render(this, score);
-
-        QGraphicsItem *renderedSystem = render(system, i, myViewType);
-        renderedSystem->setPos(0, height);
-        myRenderedSystems << renderedSystem;
-        myScene.addItem(renderedSystem);
-        height += renderedSystem->boundingRect().height() + SYSTEM_SPACING;
+        system->setPos(0, height);
+        myScene.addItem(system);
+        height += system->boundingRect().height() + SYSTEM_SPACING;
         ++i;
 
-        myCaretPainter->addSystemRect(renderedSystem->sceneBoundingRect());
+        myCaretPainter->addSystemRect(system->sceneBoundingRect());
     }
 
     myScene.addItem(myCaretPainter);
 
-    progressDialog.setValue(i);
-
-    qDebug() << "Score rendered in" << timer.elapsed() << "seconds";
+    auto end = std::chrono::high_resolution_clock::now();
+    qDebug() << "Score rendered in"
+             << std::chrono::duration_cast<std::chrono::milliseconds>(
+                    end - start).count() << "ms";
     qDebug() << "Rendered " << myScene.items().size() << "items";
 }
 
@@ -97,8 +120,7 @@ void ScoreArea::redrawSystem(int index)
 
     const Score &score = myDocument->getScore();
     SystemRenderer render(this, score);
-    QGraphicsItem *newSystem = render(score.getSystems()[index], index,
-                                        myViewType);
+    QGraphicsItem *newSystem = render(score.getSystems()[index], index, myViewType);
 
     double height = 0;
     if (index > 0)

source/app/powertabeditor.cpp
@@ -82,7 +82,7 @@
 #include <audio/midiplayer.h>
 
 #include <boost/lexical_cast.hpp>
-#include <boost/timer.hpp>
+#include <chrono>
 
 #include <dialogs/alterationofpacedialog.h>
 #include <dialogs/alternateendingdialog.h>
@@ -219,7 +219,7 @@ void PowerTabEditor::openFile(QString filename)
     if (filename.isEmpty())
         return;
 
-    boost::timer timer;
+    auto start = std::chrono::high_resolution_clock::now();
     qDebug() << "Opening file: " << filename;
 
     QFileInfo fileInfo(filename);
@@ -237,7 +237,10 @@ void PowerTabEditor::openFile(QString filename)
     if (myFileFormatManager->importFile(doc.getScore(), filename.toStdString(),
                                         *format, this))
     {
-        qDebug() << "File loaded in" << timer.elapsed() << "seconds";
+        auto end = std::chrono::high_resolution_clock::now();
+        qDebug() << "File loaded in"
+                 << std::chrono::duration_cast<std::chrono::milliseconds>(
+                        end - start).count() << "ms";
 
         doc.setFilename(filename.toStdString());
         setPreviousDirectory(filename);
@@ -2470,7 +2473,7 @@ void PowerTabEditor::setPreviousDirectory(const QString &fileName)
 
 void PowerTabEditor::setupNewTab()
 {
-    boost::timer timer;
+    auto start = std::chrono::high_resolution_clock::now();
     qDebug() << "Tab creation started ...";
 
     Q_ASSERT(myDocumentManager->hasOpenDocuments());
@@ -2538,7 +2541,10 @@ void PowerTabEditor::setupNewTab()
     updateCommands();
     scorearea->setFocus();
 
-    qDebug() << "Tab opened in" << timer.elapsed() << "seconds";
+    auto end = std::chrono::high_resolution_clock::now();
+    qDebug() << "Tap opened in"
+             << std::chrono::duration_cast<std::chrono::milliseconds>(
+                    end - start).count() << "ms";
 }
 
 namespace

commit dfe8e200abfdf0e5113c2c3ca2705f1842581e29

Fix #43 - Fix crash when rendering a tempo marker.
I think this was caused by d904aaf1 - it appears that QStaticText
doesn't like to be created from a different thread than the main UI
thread. We don't really need QStaticText there anyways, so the simplest
fix is to remove it.

source/app/scorearea.cpp
@@ -70,7 +70,7 @@ void ScoreArea::renderDocument(const Document &document, Staff::ViewType view)
 
     std::vector<std::thread> workers;
     const int workSize = myRenderedSystems.size() / numThreads;
-    qDebug() << "Using " << numThreads << " worker thread(s)";
+    qDebug() << "Using" << numThreads << "worker thread(s)";
 
     for (int i = 0; i < numThreads; ++i)
     {

source/painters/tempomarkerpainter.cpp
@@ -49,6 +49,7 @@ TempoMarkerPainter::TempoMarkerPainter(const TempoMarker &tempo)
 
         const QString imageSpacing(3, ' ');
         // Reserve space for the beat type image.
+        const int HEIGHT_OFFSET = 2;
         myBeatTypeRect =
             QRectF(fm.width(text), -HEIGHT_OFFSET, fm.width(imageSpacing),
                    fontHeight + HEIGHT_OFFSET);
@@ -57,8 +58,7 @@ TempoMarkerPainter::TempoMarkerPainter(const TempoMarker &tempo)
         text += QString::number(myTempoMarker.getBeatsPerMinute());
     }
 
-    myDisplayText.setText(text);
-    myDisplayText.prepare(QTransform(), myDisplayFont);
+    myDisplayText = text;
 
     myBounds = QRectF(0, 0, fm.width(text), fontHeight);
 }
@@ -68,7 +68,7 @@ void TempoMarkerPainter::paint(QPainter *painter,
 {
     painter->setRenderHints(QPainter::SmoothPixmapTransform);
     painter->setFont(myDisplayFont);
-    painter->drawStaticText(0, HEIGHT_OFFSET, myDisplayText);
+    painter->drawText(0, 10, myDisplayText);
 
     if (myTempoMarker.getMarkerType() == TempoMarker::StandardMarker)
     {

source/painters/tempomarkerpainter.h
@@ -20,7 +20,6 @@
 
 #include <QFont>
 #include <QGraphicsItem>
-#include <QStaticText>
 
 class TempoMarker;
 
@@ -42,11 +41,11 @@ private:
 
     const TempoMarker &myTempoMarker;
     QFont myDisplayFont;
-    QStaticText myDisplayText;
+    QString myDisplayText;
     QRectF myBeatTypeRect;
     QRectF myBounds;
 
-    static const int HEIGHT_OFFSET = 2;
+    static const int HEIGHT_OFFSET = 10;
 };
 
 #endif