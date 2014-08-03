4ed56da4a1beda9c0b2b4599ffe4c2a23da6283c
Cameron White <cameronwhite91@gmail.com>
2014-08-03 11:31:27 -0400

    Split the Linux and OSX setup commands into separate files.

---
+++ .travis/setup_linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+sudo add-apt-repository --yes ppa:kalakris/cmake # For cmake 2.8.9+
+sudo add-apt-repository --yes ppa:beineri/opt-qt521 # For Qt 5.2.1
+sudo add-apt-repository --yes ppa:boost-latest/ppa # Updated Boost
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test # gcc 4.8
+sudo apt-get update -yqq
+sudo apt-get install -yqq cmake qt52base libboost1.55-dev libboost-date-time1.55-dev libboost-iostreams1.55-dev libboost-program-options1.55-dev libasound2-dev g++-4.8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+sudo pip install cpp-coveralls pyyaml # code coverage
---
+++ .travis/setup_osx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+brew update
+brew install cmake
+brew install qt5
+brew install boost
