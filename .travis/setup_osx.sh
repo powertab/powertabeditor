5dcd2b30b4119e82912a1bfae0332feb21e698bf
Cameron White <cameronwhite91@gmail.com>
2014-10-19 20:22:15 -0400

    Fix the continuous builds.

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -3,5 +3,5 @@ set -e # Exit immediately if an error occurs.
 set -x # Echo commands.
 
 brew update
+# Install Qt. Boost is already installed on the travis environment.
 brew install qt5
-brew install boost
