d04e04c8c73415f37a590630a2578e8f3b594af3
Cameron White <cameronwhite91@gmail.com>
2014-12-30 17:04:18 -0500

    Add some debugging info to track down the OSX continuous build break.

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -5,3 +5,6 @@ set -x # Echo commands.
 brew update
 # Install Qt and binutils. Boost is already installed on the travis environment.
 brew install qt5 external/withershins/osx/binutils.rb
+
+# Debugging.
+ls /usr/local/Cellar/binutils/2.24
