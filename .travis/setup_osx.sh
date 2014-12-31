fc022f01ea1235134b115a8f0b13c17e299158f2
Cameron White <cameronwhite91@gmail.com>
2014-12-30 19:27:07 -0500

    Update withershins to add libintl dependency on OSX.

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -3,9 +3,5 @@ set -e # Exit immediately if an error occurs.
 set -x # Echo commands.
 
 brew update
-# Install Qt and binutils. Boost is already installed on the travis environment.
-brew install qt5 external/withershins/osx/binutils.rb
-
-# Debugging.
-ls -lhR /usr/local/Cellar/binutils/2.24/include
-ls -lhR /usr/local/Cellar/binutils/2.24/lib
+# Install Qt, gettex, and binutils. Boost is already installed on the travis environment.
+brew install qt5 gettext external/withershins/osx/binutils.rb
--- osx/setup.sh
+++ osx/setup.sh
@@ -3,4 +3,4 @@ set -e # Exit immediately if an error occurs.
 set -x # Echo commands.
 
 brew update
-brew install boost qt5 external/withershins/osx/binutils.rb
+brew install boost qt5 gettext external/withershins/osx/binutils.rb
