8d756c8f96631e44fc2cd5710e5240b6f1390e38
Cameron White <cameronwhite91@gmail.com>
2015-04-25 15:30:25 -0400

    We don't need to install libbfd anymore.

--- osx/setup.sh
+++ osx/setup.sh
@@ -3,5 +3,4 @@ set -e # Exit immediately if an error occurs.
 set -x # Echo commands.
 
 brew update
-brew install boost qt5 gettext external/withershins/osx/binutils.rb
-brew link --force gettext
+brew install boost qt5
