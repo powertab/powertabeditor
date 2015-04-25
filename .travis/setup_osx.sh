7932f1165322bfc98dc4b48806c856c12c41dff5
Cameron White <cameronwhite91@gmail.com>
2015-04-25 15:27:46 -0400

    Update to withershins v0.1.
    This allows us to avoid the massive libbfd dependency on OSX, at the expense
    of losing file names / line numbers for stack traces.

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -3,6 +3,5 @@ set -e # Exit immediately if an error occurs.
 set -x # Echo commands.
 
 brew update
-# Install Qt, gettex, and binutils. Boost is already installed on the travis environment.
-brew install qt5 gettext external/withershins/osx/binutils.rb
-brew link --force gettext
+# Install Qt. Boost is already installed on the travis environment.
+brew install qt5
