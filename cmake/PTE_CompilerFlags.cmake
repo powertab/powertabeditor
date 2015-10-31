002efd5e8db49aa75b8955b20be6e0da7d8039c6
Cameron White <cameronwhite91@gmail.com>
2015-10-31 14:09:44 -0400

    Linux build fix.

--- cmake/PTE_CompilerFlags.cmake
+++ cmake/PTE_CompilerFlags.cmake
@@ -7,14 +7,9 @@ set( CMAKE_CXX_STANDARD 11 )
 if ( PLATFORM_WIN )
     add_definitions( -DNOMINMAX )
 else ()
-    set( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS}
-        -std=c++11
-        -Wall
-        -Wnon-virtual-dtor
-        -Wextra
-    )
+    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wnon-virtual-dtor -Wextra")
 
     if ( PLATFORM_OSX )
-        set( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -stdlib=libc++ )
+        set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++" )
     endif ()
 endif ()
