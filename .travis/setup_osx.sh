0524987b33341869407818dee4e11cfdf5ffcbda
Cameron White <cameronwhite91@gmail.com>
2015-11-02 22:41:56 -0500

    Ensure that the CMake version is updated properly on Travis.

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -3,5 +3,8 @@ set -e # Exit immediately if an error occurs.
 set -x # Echo commands.
 
 brew update
+# Install an updated CMake version.
+brew unlink cmake
+brew install cmake
 # Install Qt. Boost is already installed on the travis environment.
-brew install qt5 cmake
+brew install qt5
