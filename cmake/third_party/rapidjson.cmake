047e7d584ed3bc82e4e05afde1c8246d7d557269
Cameron White <cameronwhite91@gmail.com>
2015-11-28 00:41:08 -0600

    Use the system version of RapidJSON if available.
    Since the Ubuntu package doesn't ship with a CMake find module, one is
    included here.
    
    Bug: #201
    Bug: #178

--- cmake/third_party/rapidjson.cmake
+++ cmake/third_party/rapidjson.cmake
@@ -1,6 +1,16 @@
+find_package( rapidjson )
+if ( RAPIDJSON_FOUND )
+    set( _rapidjson_includedirs ${RAPIDJSON_INCLUDE_DIRS} )
+else ()
+    set( _rapidjson_includedirs ${PTE_EXTERNAL_DIR}/rapidjson/include )
+    message( STATUS "Using rapidjson headers from ${_rapidjson_includedirs}" )
+endif ()
+
 add_library( rapidjson INTERFACE IMPORTED )
 
 set_target_properties(
     rapidjson PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${PTE_EXTERNAL_DIR}/rapidjson/include
+    INTERFACE_INCLUDE_DIRECTORIES ${_rapidjson_includedirs}
 )
+
+unset( _rapidjson_includedirs )
