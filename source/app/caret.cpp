powertab/powertabeditor

commit 586cd849c3ac7cde8f8d88a0caeb8fdd2ea36950

Add support to the caret for skipping hidden staves.
Bug: #66

source/app/caret.cpp
@@ -17,13 +17,13 @@
 
 #include "caret.h"
 
+#include <app/viewoptions.h>
 #include <boost/algorithm/clamp.hpp>
 #include <score/score.h>
 #include <score/system.h>
 
-Caret::Caret(Score &score)
-    : myLocation(score),
-      myInPlaybackMode(false)
+Caret::Caret(Score &score, const ViewOptions &options)
+    : myLocation(score), myViewOptions(options), myInPlaybackMode(false)
 {
 }
 
@@ -95,11 +95,28 @@ void Caret::moveStaff(int offset)
 
 void Caret::moveToStaff(int staff)
 {
-    myLocation.setStaffIndex(boost::algorithm::clamp(
-        staff, 0,
-        static_cast<int>(myLocation.getSystem().getStaves().size()) - 1));
+    const bool is_increasing = staff >= myLocation.getStaffIndex();
+    const int increment = is_increasing ? 1 : -1;
+    const int end =
+        is_increasing ? myLocation.getSystem().getStaves().size() : -1;
 
-    onLocationChanged();
+    const Score &score = myLocation.getScore();
+    const ViewFilter *filter =
+        myViewOptions.getFilter()
+            ? &score.getViewFilters()[*myViewOptions.getFilter()]
+            : nullptr;
+
+    // If the specified staff is hidden by the current filter, try the staves
+    // before or after in that direction.
+    for (int i = staff; i != end; i += increment)
+    {
+        if (!filter || filter->accept(score, myLocation.getSystemIndex(), i))
+        {
+            myLocation.setStaffIndex(i);
+            onLocationChanged();
+            return;
+        }
+    }
 }
 
 bool Caret::moveToNextBar()

source/app/caret.h
@@ -21,11 +21,13 @@
 #include <boost/signals2/signal.hpp>
 #include <score/scorelocation.h>
 
+class ViewOptions;
+
 /// Tracks the current location within the score.
 class Caret
 {
 public:
-    Caret(Score &score);
+    Caret(Score &score, const ViewOptions &options);
 
     ScoreLocation &getLocation();
     const ScoreLocation &getLocation() const;
@@ -66,9 +68,6 @@ public:
     /// Moves up or down by the given number of staves.
     void moveStaff(int offset);
 
-    /// Move to the specified staff.
-    void moveToStaff(int staff);
-
     /// Moves the caret to the next bar after the current position, or to the
     /// next system if necessary.
     bool moveToNextBar();
@@ -88,12 +87,16 @@ public:
             const LocationChangedSlot::slot_type &subscriber) const;
 
 private:
+    /// Move to the specified staff.
+    void moveToStaff(int staff);
+
     /// Returns the last valid position in the system.
     int getLastPosition() const;
     /// Returns the last valid system index in the score.
     int getLastSystemIndex() const;
 
     ScoreLocation myLocation;
+    const ViewOptions &myViewOptions;
     bool myInPlaybackMode;
 
     /// Send out signals to subscribers whenever the location changes.

source/app/documentmanager.cpp
@@ -121,7 +121,7 @@ int DocumentManager::getCurrentDocumentIndex() const
 }
 
 Document::Document()
-    : myCaret(myScore)
+    : myCaret(myScore, myViewOptions)
 {
 }
 

source/score/utils/scoremerger.cpp
@@ -17,6 +17,7 @@
 
 #include "scoremerger.h"
 
+#include <app/viewoptions.h>
 #include <score/score.h>
 #include <score/utils.h>
 #include <score/voiceutils.h>
@@ -24,10 +25,11 @@
 
 /// Approximate upper limit on the number of positions in a system.
 static const int POSITION_LIMIT = 30;
+static const ViewOptions theDefaultViewOptions;
 
 ScoreMerger::ScoreMerger(Score &dest, Score &guitarScore, Score &bassScore)
     : myDestScore(dest),
-      myDestCaret(dest),
+      myDestCaret(dest, theDefaultViewOptions),
       myDestLoc(myDestCaret.getLocation()),
       myGuitarScore(guitarScore),
       myBassScore(bassScore),
@@ -588,7 +590,7 @@ void ScoreMerger::merge()
 }
 
 ScoreMerger::State::State(Score &score, bool isBass)
-    : caret(score),
+    : caret(score, theDefaultViewOptions),
       repeatIndex(score),
       loc(caret.getLocation()),
       isBass(isBass),
@@ -722,7 +724,8 @@ static int getRepeatedSectionWidth(Score &score, const RepeatedSection &sec)
     auto &startLoc = sec.getStartBarLocation();
     auto &endLoc = sec.getLastEndBarLocation();
 
-    Caret caret(score);
+    ViewOptions view_options;
+    Caret caret(score, view_options);
     caret.moveToSystem(startLoc.getSystem(), true);
     caret.moveToPosition(startLoc.getPosition());
 

commit cb7d429e1e8b1db214840dfd66aebb6d56161a21

Fix crash introduced in 586cd849 when removing the last staff of a system.
Fixes: #216

source/app/caret.cpp
@@ -95,10 +95,13 @@ void Caret::moveStaff(int offset)
 
 void Caret::moveToStaff(int staff)
 {
+    const int num_staves =
+        static_cast<int>(myLocation.getSystem().getStaves().size());
+    staff = boost::algorithm::clamp(staff, 0, num_staves - 1);
+
     const bool is_increasing = staff >= myLocation.getStaffIndex();
     const int increment = is_increasing ? 1 : -1;
-    const int end =
-        is_increasing ? myLocation.getSystem().getStaves().size() : -1;
+    const int end = is_increasing ? num_staves : -1;
 
     const Score &score = myLocation.getScore();
     const ViewFilter *filter =