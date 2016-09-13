902f0b799cf1403ac5d9bc3d55f3f459efdb7b56
PellTheTabber1 <jdb_one@yahoo.com>
2016-09-13 05:34:01 -0700

    Update powertabeditor.h

--- powertabeditor.h
+++ powertabeditor.h
@@ -192,9 +192,9 @@ private slots:
     /// Updates the duration of the current note and sets the default duration
     /// for new notes.
     void updateNoteDuration(Position::DurationType duration);
+    /// Increase or decrease the current note's duration to match it's bend duration.
+    void updateNoteDurationForBend(const ScoreLocation &location, const Note *note);
     /// Increase or decrease the current note duration.
-	void updateNoteDurationForBend(const ScoreLocation &location, const Note *note);
-	/// Increase or decrease the current note's duration to match it's bend duration.
     void changeNoteDuration(bool increase);
     /// Adds a dot to the current position's duration.
     void addDot();
