d979d8324be1045dd19f99c09335108f0b5a37a1
Cameron White <cameronwhite91@gmail.com>
2016-10-10 21:11:11 -0400

    Fix the travis OSX build.
    With the recent OSX environment changes, a recent CMake version now
    seems to be installed by default.

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -3,8 +3,5 @@ set -e # Exit immediately if an error occurs.
 set -x # Echo commands.
 
 brew update
-# Install an updated CMake version.
-brew unlink cmake
-brew install cmake
-# Install Qt. Boost is already installed on the travis environment.
+# Install Qt. Boost and cmake are already installed on the travis environment.
 brew install qt5
