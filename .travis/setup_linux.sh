e10f0a7fe9c738c4a6a571839d00cb399eb1c134
Cameron White <cameronwhite91@gmail.com>
2017-05-06 15:29:45 -0400

    Install libiberty-dev when building on Linux.

--- .travis/setup_linux.sh
+++ .travis/setup_linux.sh
@@ -4,4 +4,4 @@ set -x # Echo commands.
 
 sudo add-apt-repository --yes ppa:beineri/opt-qt542-trusty # For Qt 5.4.2
 sudo apt-get update -yqq
-sudo apt-get install -yqq cmake qt54base libboost-dev libboost-date-time-dev libboost-filesystem-dev libboost-iostreams-dev libboost-program-options-dev libboost-regex-dev libasound2-dev binutils-dev
+sudo apt-get install -yqq cmake qt54base libboost-dev libboost-date-time-dev libboost-filesystem-dev libboost-iostreams-dev libboost-program-options-dev libboost-regex-dev libasound2-dev libiberty-dev binutils-dev
