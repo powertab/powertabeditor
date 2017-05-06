e5e2b47cd07ba1c1373dffdd2061fb85077df194
Cameron White <cameronwhite91@gmail.com>
2017-05-06 15:04:05 -0400

    Fix the Qt 5.4 PPA.

--- .travis/setup_linux.sh
+++ .travis/setup_linux.sh
@@ -2,6 +2,6 @@
 set -e # Exit immediately if an error occurs.
 set -x # Echo commands.
 
-sudo add-apt-repository --yes ppa:beineri/opt-qt542 # For Qt 5.4.2
+sudo add-apt-repository --yes ppa:beineri/opt-qt542-trusty # For Qt 5.4.2
 sudo apt-get update -yqq
 sudo apt-get install -yqq cmake qt54base libboost-dev libboost-date-time-dev libboost-filesystem-dev libboost-iostreams-dev libboost-program-options-dev libboost-regex-dev libasound2-dev binutils-dev
