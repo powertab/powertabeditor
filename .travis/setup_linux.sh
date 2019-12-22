3a9bb4aa4ff96925df8bd161d7e32592801af50d
Cameron White <cameronwhite91@gmail.com>
2019-12-21 23:18:26 -0500

    Remove old travis code and install more dependencies.

--- .travis/setup_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e # Exit immediately if an error occurs.
-set -x # Echo commands.
-
-sudo apt-get update -yqq
-sudo apt-get install -yqq cmake qtbase5-dev libboost-dev libboost-date-time-dev libboost-filesystem-dev libboost-iostreams-dev libboost-program-options-dev libboost-regex-dev libasound2-dev librapidjson-dev ninja-build
