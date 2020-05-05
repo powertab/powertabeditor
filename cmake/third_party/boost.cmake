6663b13477b10de170ed60ffe5e598ad01d56d85
Cameron White <cameronwhite91@gmail.com>
2020-05-05 18:28:42 -0400

    Fix issues with building against newer Boost versions
    We now use the imported targets that are provided by BoostConfig.cmake. For older versions, the targets are now created with different names to match the new upstream behaviour.
    
    Bug: #282

--- cmake/third_party/boost.cmake
+++ cmake/third_party/boost.cmake
@@ -16,61 +16,67 @@ find_package(
     COMPONENTS ${_boost_libs}
 )
 
-# Set up imported targets.
-add_library( boost INTERFACE IMPORTED )
-set_target_properties(
-    boost PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIR}
-    INTERFACE_COMPILE_DEFINITIONS "BOOST_ALL_DYN_LINK;BOOST_ALL_NO_LIB"
-)
+# Set up imported targets if not already provided.
+if ( NOT TARGET Boost::headers )
+    add_library( Boost::headers INTERFACE IMPORTED )
+    set_target_properties(
+        Boost::headers PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIR}
+        INTERFACE_COMPILE_DEFINITIONS "BOOST_ALL_DYN_LINK;BOOST_ALL_NO_LIB"
+    )
+endif ()
 
 function ( pte_boost_library libname )
     string( TOUPPER ${libname} libname_upper )
-    
-    # Figure out the .dll paths.
-    
-    set( implib_release ${Boost_${libname_upper}_LIBRARY_RELEASE} )
-    set( implib_debug ${Boost_${libname_upper}_LIBRARY_DEBUG} )
-    if ( NOT implib_debug )
-        set( implib_debug ${implib_release} )
-    endif ()
-    
-    get_filename_component( boost_dir ${implib_debug} DIRECTORY )
-    get_filename_component( basename_debug ${implib_debug} NAME_WE )
-    get_filename_component( basename_release ${implib_release} NAME_WE )
 
-    set( lib_debug ${boost_dir}/${basename_debug}${CMAKE_SHARED_LIBRARY_SUFFIX} )
-    set( lib_release ${boost_dir}/${basename_release}${CMAKE_SHARED_LIBRARY_SUFFIX} )
+    # Newer Boost versions already provide an imported target.
+    if ( NOT TARGET Boost::${libname} )
+        # Figure out the .dll paths.
+        set( implib_release ${Boost_${libname_upper}_LIBRARY_RELEASE} )
+        set( implib_debug ${Boost_${libname_upper}_LIBRARY_DEBUG} )
+        if ( NOT implib_debug )
+            set( implib_debug ${implib_release} )
+        endif ()
 
-    add_library( boost_${libname} SHARED IMPORTED )
-    set_target_properties(
-        boost_${libname} 
-        PROPERTIES 
-        INTERFACE_LINK_LIBRARIES boost
-        IMPORTED_LOCATION_DEBUG ${lib_debug} 
-        IMPORTED_LOCATION_RELEASE ${lib_release} 
-        IMPORTED_IMPLIB_DEBUG ${implib_debug} 
-        IMPORTED_IMPLIB_RELEASE ${implib_release}
-    )
+        get_filename_component( boost_dir ${implib_debug} DIRECTORY )
+        get_filename_component( basename_debug ${implib_debug} NAME_WE )
+        get_filename_component( basename_release ${implib_release} NAME_WE )
 
-    if ( NOT PLATFORM_WIN )
+        set( lib_debug ${boost_dir}/${basename_debug}${CMAKE_SHARED_LIBRARY_SUFFIX} )
+        set( lib_release ${boost_dir}/${basename_release}${CMAKE_SHARED_LIBRARY_SUFFIX} )
+
+        add_library( Boost::${libname} SHARED IMPORTED )
         set_target_properties(
-            boost_${libname}
-            PROPERTIES
-            IMPORTED_LOCATION ${Boost_${libname_upper}_LIBRARY}
+            Boost::${libname} 
+            PROPERTIES 
+            INTERFACE_LINK_LIBRARIES Boost::headers
+            IMPORTED_LOCATION_DEBUG ${lib_debug} 
+            IMPORTED_LOCATION_RELEASE ${lib_release} 
+            IMPORTED_IMPLIB_DEBUG ${implib_debug} 
+            IMPORTED_IMPLIB_RELEASE ${implib_release}
         )
+
+        if ( NOT PLATFORM_WIN )
+            set_target_properties(
+                Boost::${libname}
+                PROPERTIES
+                IMPORTED_LOCATION ${Boost_${libname_upper}_LIBRARY}
+            )
+        endif ()
+
+        if ( ${libname} STREQUAL "filesystem" )
+            # boost_filesystem depends on boost_system
+            set_target_properties(
+                Boost::filesystem
+                PROPERTIES
+                INTERFACE_LINK_LIBRARIES Boost::system
+            )
+        endif ()
     endif ()
 endfunction ()
 
 foreach ( lib ${_boost_libs} )
     pte_boost_library( ${lib} )
 endforeach ()
-
-# boost_filesystem depends on boost_system
-set_target_properties(
-    boost_filesystem
-    PROPERTIES 
-    INTERFACE_LINK_LIBRARIES boost_system
-)
 
 unset( _boost_libs )
