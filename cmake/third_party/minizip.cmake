a9af2a5ffd1f0063288fe8c135e351016c4ca169
Cameron White <cameronwhite91@gmail.com>
2020-05-28 19:37:50 -0700

    Get minizip working correctly on Windows.

--- cmake/third_party/minizip.cmake
+++ cmake/third_party/minizip.cmake
@@ -5,3 +5,11 @@ target_include_directories( minizip::minizip INTERFACE ${minizip_INCLUDE_DIRS} )
 target_link_libraries( minizip::minizip
     INTERFACE ${minizip_LIBRARIES}
 )
+
+if ( PLATFORM_WIN )
+    # On Windows, linking against zlib also seems to be required.
+    find_package( ZLIB REQUIRED )
+    target_link_libraries( minizip::minizip
+        INTERFACE ${ZLIB_LIBRARIES}
+    )
+endif ()
