a361caa2e824cff77dc170fc720e1408caa6746b
Cameron White <cameronwhite91@gmail.com>
2020-06-01 07:48:17 -0400

    Remove unnecessary RtMidi configuration.
    This shouldn't be needed now that RtMidi is built out of the source tree.
    Moved the CoreMIDI dependency into the audio directory, since it's used for the OSX software synth.

--- cmake/third_party/rtmidi.cmake
+++ cmake/third_party/rtmidi.cmake
@@ -1,30 +1,6 @@
-# Determine the platform-specific libraries to link against.
-if ( PLATFORM_WIN )
-    set( _midi_libs winmm )
-    set( _midi_defs __WINDOWS_MM__ )
-elseif ( PLATFORM_OSX )
-    find_library( coreaudio_lib CoreAudio )
-    find_library( coremidi_lib CoreMIDI )
-    find_library( corefoundation_lib CoreFoundation )
-
-    set( _midi_libs
-        ${coreaudio_lib}
-        ${coremidi_lib}
-        ${corefoundation_lib}
-    )
-    set( _midi_defs __MACOSX_CORE__ )
-elseif ( PLATFORM_LINUX )
-    find_package( ALSA REQUIRED )
-
-    set( _midi_libs ${ALSA_LIBRARY} pthread )
-    set( _midi_defs __LINUX_ALSA__ )
-endif ()
-
 find_package( rtmidi REQUIRED )
 
 add_library( rtmidi::rtmidi IMPORTED SHARED )
-target_link_libraries( rtmidi::rtmidi INTERFACE ${_midi_libs} )
-target_compile_definitions( rtmidi::rtmidi INTERFACE ${_midi_defs} )
 target_include_directories( rtmidi::rtmidi INTERFACE ${rtmidi_INCLUDE_DIRS} )
 
 if ( PLATFORM_WIN )
@@ -39,6 +15,3 @@ else ()
         IMPORTED_LOCATION ${rtmidi_LIBRARIES}
     )
 endif ()
-
-unset( _midi_libs )
-unset( _midi_defs )
