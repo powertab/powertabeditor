c20951904dd49b211500da7540d32047dc723892
Cameron White <cameronwhite91@gmail.com>
2020-06-01 08:07:48 -0400

    Restore linking against pthread when using rtmidi on Linux.

--- cmake/third_party/rtmidi.cmake
+++ cmake/third_party/rtmidi.cmake
@@ -3,6 +3,10 @@ find_package( rtmidi REQUIRED )
 add_library( rtmidi::rtmidi IMPORTED SHARED )
 target_include_directories( rtmidi::rtmidi INTERFACE ${rtmidi_INCLUDE_DIRS} )
 
+if ( PLATFORM_LINUX )
+    target_link_libraries( rtmidi::rtmidi INTERFACE pthread )
+endif ()
+
 if ( PLATFORM_WIN )
     set_target_properties(
         rtmidi::rtmidi PROPERTIES
