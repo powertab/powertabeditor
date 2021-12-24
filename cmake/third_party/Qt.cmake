3d9925a75cb9bdb1ffa85cd96d51b70230a219b8
Cameron White <cameronwhite91@gmail.com>
2021-12-24 14:57:27 -0500

    Refactor handling of Qt translations
    This is reused for Windows, where we need to the copy the Qt installation's translations into our build folder (where the dlls are also copied)

--- cmake/third_party/Qt.cmake
+++ cmake/third_party/Qt.cmake
@@ -7,3 +7,14 @@ set( QT5_PLUGINS )
 if ( PLATFORM_WIN )
 	set( QT5_PLUGINS Qt5::QWindowsIntegrationPlugin )
 endif ()
+
+# Find some standard paths to the Qt installation.
+get_target_property( _uic_path Qt5::uic IMPORTED_LOCATION )
+get_filename_component( PTE_QT_BIN_DIR ${_uic_path} DIRECTORY)
+cmake_path( GET PTE_QT_BIN_DIR PARENT_PATH PTE_QT_ROOT_DIR )
+
+# Find the Qt translations, which we need to copy for Windows / Mac installers.
+cmake_path( APPEND PTE_QT_ROOT_DIR "translations" OUTPUT_VARIABLE PTE_QT_TRANSLATIONS_DIR )
+file( GLOB PTE_QT_TRANSLATION_FILES
+    ${PTE_QT_TRANSLATIONS_DIR}/qtbase_*.qm
+)
