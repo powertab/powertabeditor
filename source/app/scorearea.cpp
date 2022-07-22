powertab/powertabeditor

commit 9a513906b43b6727ca093c91135a2cb014fe68d3

Add custom system spacing (#382)
System spacing can now be set in Preferences->General tab

source/app/scorearea.cpp
@@ -33,8 +33,6 @@
 #include <QScrollBar>
 #include <score/score.h>
 
-static const double SYSTEM_SPACING = 50;
-
 void ScoreArea::Scene::dragEnterEvent(QGraphicsSceneDragDropEvent *event)
 {
     event->ignore();
@@ -66,8 +64,13 @@ ScoreArea::ScoreArea(SettingsManager &settings_manager, QWidget *parent)
     // Load the user's preferred theme, and re-render when the theme setting
     // changes.
     loadTheme(settings_manager, /* redraw */ false);
+    loadSystemSpacing(settings_manager, false);
     mySettingsListener = settings_manager.subscribeToChanges(
-        [&]() { loadTheme(settings_manager); });
+        [&]()
+        {
+            loadTheme(settings_manager);
+            loadSystemSpacing(settings_manager);
+        });
 
     // Connect the click event handler to our public signals.
     myClickEvent.connect(
@@ -135,11 +138,11 @@ void ScoreArea::renderDocument(const Document &document)
     double height = 0;
     // Score info.
     myScene.addItem(myScoreInfoBlock);
-    height += myScoreInfoBlock->boundingRect().height() + 0.5 * SYSTEM_SPACING;
+    height += myScoreInfoBlock->boundingRect().height() + 0.5 * mySystemSpacing;
 
     myChordDiagramList->setPos(0, height);
     myScene.addItem(myChordDiagramList);
-    height += myChordDiagramList->boundingRect().height() + 0.5 * SYSTEM_SPACING;
+    height += myChordDiagramList->boundingRect().height() + 0.5 * mySystemSpacing;
     myHeaderSize = height;
 
     // Layout the systems.
@@ -147,7 +150,7 @@ void ScoreArea::renderDocument(const Document &document)
     {
         system->setPos(0, height);
         myScene.addItem(system);
-        height += system->boundingRect().height() + SYSTEM_SPACING;
+        height += system->boundingRect().height() + mySystemSpacing;
 
         myCaretPainter->addSystemRect(system->sceneBoundingRect());
     }
@@ -174,13 +177,13 @@ void ScoreArea::redrawSystem(int index)
     if (index > 0)
     {
         height = myRenderedSystems.at(index - 1)->sceneBoundingRect().bottom() +
-                SYSTEM_SPACING;
+                mySystemSpacing;
     }
     else
         height = myHeaderSize;
 
     newSystem->setPos(0, height);
-    height += newSystem->boundingRect().height() + SYSTEM_SPACING;
+    height += newSystem->boundingRect().height() + mySystemSpacing;
     myCaretPainter->setSystemRect(index, newSystem->sceneBoundingRect());
 
     myScene.addItem(newSystem);
@@ -191,7 +194,7 @@ void ScoreArea::redrawSystem(int index)
     {
         QGraphicsItem *system = myRenderedSystems[i];
         system->setPos(0, height);
-        height += system->boundingRect().height() + SYSTEM_SPACING;
+        height += system->boundingRect().height() + mySystemSpacing;
         myCaretPainter->setSystemRect(i, system->sceneBoundingRect());
     }
 
@@ -353,4 +356,13 @@ ScoreArea::loadTheme(const SettingsManager &settings_manager, bool redraw)
         setPalette(*myActivePalette);
         myDisableRedraw = false;
     }
+}
+
+void
+ScoreArea::loadSystemSpacing(const SettingsManager &settings_manager, bool redraw)
+{
+    auto settings = settings_manager.getReadHandle();
+    mySystemSpacing = settings->get(Settings::SystemSpacing);
+    if (redraw)
+        this->renderDocument(*myDocument);
 }

source/app/scorearea.h
@@ -80,12 +80,14 @@ private:
 
     /// Load the user's preferred color scheme for the score.
     void loadTheme(const SettingsManager &settings_manager, bool redraw = true);
+    void loadSystemSpacing(const SettingsManager &settings_manager, bool redraw = true);
 
     Scene myScene;
     const Document *myDocument;
     QGraphicsItem *myScoreInfoBlock;
     QGraphicsItem *myChordDiagramList;
     double myHeaderSize = 0;
+    double mySystemSpacing = 0;
     QList<QGraphicsItem *> myRenderedSystems;
     CaretPainter *myCaretPainter;
     /// The color palette from the parent widget.

source/app/settings.cpp
@@ -39,6 +39,8 @@ const Setting<std::vector<std::string>> RecentFiles("app/recent_files", {});
 const Setting<bool> OpenFilesInNewWindow("app/open_files_in_new_window",
                                          false);
 
+const Setting<int> SystemSpacing("app/system_spacing", 50);
+
 const Setting<ScoreTheme> Theme("app/score_theme", ScoreTheme::SystemDefault);
 
 const Setting<std::string> DefaultInstrumentName("app/default_instrument_name",

source/app/settings.h
@@ -42,6 +42,7 @@ namespace Settings
     extern const Setting<std::vector<std::string>> RecentFiles;
     extern const Setting<ScoreTheme> Theme;
     extern const Setting<bool> OpenFilesInNewWindow;
+    extern const Setting<int> SystemSpacing;
 
     extern const Setting<std::string> DefaultInstrumentName;
     extern const Setting<int> DefaultInstrumentPreset;

source/dialogs/preferencesdialog.cpp
@@ -73,6 +73,8 @@ PreferencesDialog::PreferencesDialog(QWidget *parent,
 
     ui->countInVolumeSpinBox->setRange(0, 127);
 
+    ui->systemSpacingSpinBox->setRange(0, 100);
+
     loadCurrentSettings();
 }
 
@@ -131,6 +133,9 @@ void PreferencesDialog::loadCurrentSettings()
     ui->openInNewWindowCheckBox->setChecked(
         settings->get(Settings::OpenFilesInNewWindow));
 
+    ui->systemSpacingSpinBox->setValue(
+      settings->get(Settings::SystemSpacing));
+
     ui->defaultInstrumentNameLineEdit->setText(
         QString::fromStdString(settings->get(Settings::DefaultInstrumentName)));
     ui->defaultPresetComboBox->setCurrentIndex(
@@ -188,6 +193,9 @@ void PreferencesDialog::accept()
 
     settings->set(Settings::OpenFilesInNewWindow,
                   ui->openInNewWindowCheckBox->isChecked());
+
+    settings->set(Settings::SystemSpacing,
+                  ui->systemSpacingSpinBox->value());
 
     settings->set(Settings::DefaultInstrumentName,
                   ui->defaultInstrumentNameLineEdit->text().toStdString());

source/dialogs/preferencesdialog.ui
@@ -275,6 +275,16 @@
               </item>
              </widget>
             </item>
+            <item row="2" column="0">
+              <widget class="QLabel" name="systemSpacingLabel">
+                <property name="text">
+                  <string>System Spacing:</string>
+                </property>
+              </widget>
+            </item>
+            <item row="2" column="1">
+              <widget class="QSpinBox" name="systemSpacingSpinBox"/>
+            </item>
            </layout>
           </item>
          </layout>

commit b67ce59a172021f280f5d50136f1f1ec0a3e5b73

Fix a regression from #382 that caused excessive redraws of scores
Any settings change (e.g. when opening a new file, the last used directory is updated in the settings) caused all open scores to be redrawn. Instead, this should only happen if the value we're interested in actually changed.

source/app/scorearea.cpp
@@ -362,7 +362,8 @@ void
 ScoreArea::loadSystemSpacing(const SettingsManager &settings_manager, bool redraw)
 {
     auto settings = settings_manager.getReadHandle();
+    const double prev_spacing = mySystemSpacing;
     mySystemSpacing = settings->get(Settings::SystemSpacing);
-    if (redraw)
+    if (redraw && mySystemSpacing != prev_spacing)
         this->renderDocument(*myDocument);
 }