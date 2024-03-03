6c638fd906863f5ab8ea2722ecc2445812648c1f
dj-viking <anders.swedishviking@gmail.com>
2024-03-02 21:05:56 -0500

    update cmake-build script

--- cmake-build.ps1
+++ cmake-build.ps1
@@ -3,9 +3,15 @@ param(
     $vcpkg_dir = "C:\Program Files\vcpkg\"
 )
 
-$qt5_widgets_cmake_path = "$($PSScriptRoot)/Qt/5.15.2/msvc2019_64/lib/cmake/Qt5Widgets/";
-$qt5_network_cmake_path = "$($PSScriptRoot)/Qt/5.15.2/msvc2019_64/lib/cmake/Qt5Network/";
-$qt5_printsupport_cmake_path = "$($PSScriptRoot)/Qt/5.15.2/msvc2019_64/lib/cmake/Qt5PrintSupport/";
-$qt5_linguisttools_cmake_path = "$($PSScriptRoot)/Qt/5.15.2/msvc2019_64/lib/cmake/Qt5LinguistTools";
+$qt_path = "$($PSScriptRoot)/Qt";
+
+if (!(Test-Path "$qt_path")) {
+    throw "Qt dependency is not installed yet, `\n for this script to work install Qt to this project's root directory or change the path to your Qt installation";
+}
+
+$qt5_widgets_cmake_path = "$($qt_path)/5.15.2/msvc2019_64/lib/cmake/Qt5Widgets/";
+$qt5_network_cmake_path = "$($qt_path)/5.15.2/msvc2019_64/lib/cmake/Qt5Network/";
+$qt5_printsupport_cmake_path = "$($qt_path)/5.15.2/msvc2019_64/lib/cmake/Qt5PrintSupport/";
+$qt5_linguisttools_cmake_path = "$($qt_path)/5.15.2/msvc2019_64/lib/cmake/Qt5LinguistTools";
 
 cmake -A x64 -B ./build -DPTE_ENABLE_PCH=1 -DCMAKE_TOOLCHAIN_FILE="$($vcpkg_dir)/scripts/buildsystems/vcpkg.cmake" -DCMAKE_PREFIX_PATH="$qt5_widgets_cmake_path;$qt5_network_cmake_path;$qt5_printsupport_cmake_path;$qt5_linguisttools_cmake_path"
