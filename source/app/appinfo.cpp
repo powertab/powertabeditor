dcc4d511ceb6cf0a9cab2a33866d8b1ab1e92d02
Cameron White <cameronwhite91@gmail.com>
2025-06-29 12:41:32 -0400

    Fix incorrect version number which was missed in 13cab27c

--- source/app/appinfo.cpp
+++ source/app/appinfo.cpp
@@ -27,7 +27,7 @@ namespace AppInfo
     const char *TRANSLATIONS_URL =
         "https://hosted.weblate.org/engage/powertabeditor";
     const char *APPLICATION_NAME = "Power Tab Editor";
-    const char *APPLICATION_VERSION = "2.0.21";
+    const char *APPLICATION_VERSION = "2.0.22";
 
 #if defined(Q_OS_WIN)
     const char *ORGANIZATION_NAME = "Power Tab";
